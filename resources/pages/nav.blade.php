0c34d67493847b53014a49e659380b49a547ecb2
ahmed kotb <ahmedmohamed.kotb@yahoo.com>
2016-02-11 17:06:22 +0200

    ahmed take care of this edits

---
+++ resources/pages/nav.blade.php
@@ -0,0 +1,113 @@
+{{--This appear only when large or mideum screen--}}
+<div class="row hidden-sm hidden-xs">
+    <div class="col-xs-9">
+        <div class="row">
+            <div class="col-xs-12 col-sm-7 col-md-8 col-lg-10 pull-left" style="padding-top:20px;">
+
+                <a href=""> <img src="/images/youtube.png"></a>
+                <a href=""><img src="/images/facebook.png"></a>
+                <a href=""><img src="/images/twitter.png"></a>
+                <a href="" class="button" style=" border-right:1px solid black !important; padding-right:5px; ">Log
+                    in </a>
+                <a href="" class="button"> Register </a>
+
+                <form class="navbar-form" role="search">
+                    <div class="input-group">
+                        <div class="input-group-btn">
+                            <button class="btn btn-default btn-circle" type="submit"><span
+                                        class="glyphicon glyphicon-search"></span></button>
+                        </div>
+                        <input type="text" class="form-control" name="srch-term" id="srch-term">
+                    </div>
+                </form>
+
+                <a href="" class="button" style="border-right:1px solid black !important;"> English </a>
+                <a href="" class="button" style="padding-left:7px;"> عربي </a>
+            </div>
+        </div>
+        <div class="row  hidden-xs col-md-12 col-lg-12 "
+             style="text-align:right; margin-bottom: -30px !important;">
+            <img class="headerBig-Image" src="/images/pictures/5.png">
+        </div>
+    </div>
+    <div class="col-xs-3 pull-right " style="text-align:center">
+        <img src="/images/pictures/1.png" class="logo-icon">
+    </div>
+
+</div>
+
+<div class="list  hidden-md hidden-lg">
+    <ul>
+        <li class="inner-addon">
+            <i class="glyphicon glyphicon-search"></i>
+            <input type="text" class="form-control" placeholder="Search...">
+        </li>
+        <li style="text-align: center;background-color: white">
+            <img src="/images/pictures/1.png" class="logo-icon">
+        </li>
+        <li><a href="#"> الرئيسية </a></li>
+        <li><a href="#"> تجمعاتنا </a></li>
+        <li><a href="#"> دنيانا </a></li>
+        <li><a href="#"> انضمي لنا </a></li>
+        <li><a href="#"> إتكلمى </a></li>
+        <li><a href="#"> جاليري </a></li>
+        <li><a href="#"> للاتصال بنا </a></li>
+    </ul>
+</div>
+
+<div class="close-list  hidden-md hidden-lg">
+    <span class="glyphicon glyphicon-remove close_the_list_icon"></span>
+</div>
+
+{{--This appear only when small screen--}}
+<div class="row hidden-md hidden-lg navbar-fixed-top "
+     style="z-index: 9797997; width:100%;border-bottom: 2px solid #888888; ">
+    <div class="row" style="background-color:#B0D3D9">
+        <div class="col-xs-4 col-sm-4">
+            <div class="row" style="padding-left: 20px;">
+                <div class="glyph">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-xs-5 col-sm-5" style="height:100%">
+            <div class="row" style="text-align: right;padding-right:20px;vertical-align: middle;margin-top: 5px !important;">
+
+                <h5><a href="/" class="nav_link"> Log in </a><a href="/aa" class="nav_link">& Register</a></h5>
+            </div>
+        </div>
+        <div class="col-xs-3 col-sm-3 text-center">
+            <h3 style="color: #ffffff; font-family:ebold;">إتكلمى</h3>
+        </div>
+    </div>
+</div>
+
+{{--navbar appear only in large and medium screens--}}
+<div class="navbar navbar-default navbar-static-top  hidden-xs hidden-sm" style="background-color:#376773;>
+
+    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navHeaderCollapse"
+            aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navHeaderCollapse">
+        <ul class="nav navbar-nav navbar-right"
+            style="text-align:center; width:100%; margin-right:15px!important;font-family: elight!important;">
+
+            <li><a href="#"> للاتصال بنا </a></li>
+            <li><a href="#"> جاليري </a></li>
+            <li><a href="#"> إتكلمى </a></li>
+            <li><a href="#"> انضمي لنا </a></li>
+            <li><a href="#"> دنيانا </a></li>
+            <li><a href="#"> تجمعاتنا </a></li>
+            <li><a href="#"> الرئيسية </a></li>
+        </ul>
+
+    </div>
+</div>
